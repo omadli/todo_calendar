513f3c72db5688184299f2e278596b7e48e28e73
Omadli Yigit <76625693+omadli@users.noreply.github.com>
2023-06-08 01:16:59 +0500

    fix bug!

--- calendarapp/context_processors.py
+++ calendarapp/context_processors.py
@@ -4,13 +4,14 @@ from django.http import HttpRequest
 from .models.event import Event
 
 def event_renderer(request: HttpRequest):
-    return {
-       'my_events': Event.objects.filter(
-            user=request.user,
-            is_active=True,
-            is_deleted=False,
-            end_time__gte=datetime.now().date(),
-            end_time__lte=(datetime.now().date() + timedelta(days=5)),
-        ).order_by("end_time") 
-    }
+    if request.user.is_authenticated:
+        return {
+           'my_events': Event.objects.filter(
+                user=request.user,
+                is_active=True,
+                is_deleted=False,
+                end_time__gte=datetime.now().date(),
+                end_time__lte=(datetime.now().date() + timedelta(days=5)),
+            ).order_by("end_time") 
+        }
     
